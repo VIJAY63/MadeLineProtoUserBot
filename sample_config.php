d80984461b4bb0f581e0c71edadce4933b071f0f
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-06-23 16:59:14 +0530

    Add sample config PHP to copy values

---
+++ sample_config.php
@@ -0,0 +1,6 @@
+<?php
+$GLOBALS["APP_ID"] = 6;
+$GLOBALS["API_HASH"] = "eb06d4abfb49dc3eeb1aeb98ae0f581e";
+$GLOBALS["MLP_SESSION_NAME"] = "user.madeline";
+// https://t.me/TrollVoiceBot?start=2558
+$GLOBALS["COMMAND_HANDLER"] = ".";
